56a7c5e34e5bb299f017921bd2b28b87fa26b9ec
Tanveer-Bajwa <tanveer3612138@gmail.com>
2024-11-18 22:50:16 +0500

    refactor

--- model/index.js
+++ model/index.js
@@ -1,10 +1,8 @@
-// models/index.js
 const sequelize = require('../config/db');
 const Task = require('./task');
 const User = require('./user');
 
-// Sync the models with the database
-sequelize.sync({ force: false }) // Use `force: true` only for development or initial structure changes
+sequelize.sync({ force: false })
   .then(() => console.log('Database & tables created!'))
   .catch(err => console.error('Error:', err));
 
